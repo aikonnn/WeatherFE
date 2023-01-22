0be21797bab0fbb40437e4f7f440c9fa85b05024
RyanPark0128 <vazywon@hotmail.com>
2023-01-21 16:37:53 -0800

    initilize map feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Carousel from './components/Carousel';
+import Map from './components/Map';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Carousel />
+      <Map />
     </div>
   );
 }
